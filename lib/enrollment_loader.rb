d249446da54639526ea9d4aca93f85381f847027
Matt Stjernholm <thew88@gmail.com>
2015-09-17 17:08:05 -0600

    more encapsulation

--- lib/enrollment_loader.rb
+++ lib/enrollment_loader.rb
@@ -5,23 +5,27 @@ class EnrollmentLoader
     File.expand_path '../data', __dir__
   end
 
+  def self.group_by(rows)
+    rows.group_by { |row| row.fetch(:location) }
+  end
+
+  def self.repo_builder(repo, groups)
+    groups.each_pair do |key, value|
+      repo[key.upcase] = value.map { |row| [row.fetch(:timeframe).to_i, row.fetch(:data).to_i] }.to_h
+    end
+  end
+
   def self.load_pupil_enrollment
     rows = CSV.readlines(path + '/Pupil enrollment.csv', headers: true, header_converters: :symbol).map(&:to_h)
-    groups = rows.group_by { |row| row.fetch(:location) }
-    enrollment_data = rows.map { |row| [row.fetch(:timeframe).to_i, row.fetch(:data).to_i] }.to_h
+    groups = group_by(rows)
     @enrollment_pupil_repo = {}
-    groups.each_pair do |key, value|
-      @enrollment_pupil_repo[key.upcase] = value.map { |row| [row.fetch(:timeframe).to_i, row.fetch(:data).to_i] }.to_h
-    end
+    repo_builder(@enrollment_pupil_repo, groups)
   end
 
   def self.load_online_pupil_enrollment
     rows = CSV.readlines(path + '/Online pupil enrollment.csv', headers: true, header_converters: :symbol).map(&:to_h)
-    groups = rows.group_by { |row| row.fetch(:location) }
-    enrollment_data = rows.map { |row| [row.fetch(:timeframe).to_i, row.fetch(:data).to_i] }.to_h
+    groups = group_by(rows)
     @online_enrollment_pupil_repo = {}
-    groups.each_pair do |key, value|
-      @online_enrollment_pupil_repo[key.upcase] = value.map { |row| [row.fetch(:timeframe).to_i, row.fetch(:data).to_i] }.to_h
-    end
+    repo_builder(@online_enrollment_pupil_repo, groups)
   end
 end
