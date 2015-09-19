f8c928c5a7e970e49893aeeadda3e22a67d9f9b3
Matt Stjernholm <thew88@gmail.com>
2015-09-19 12:10:06 -0600

    statewide loader parses correctly --needs truncation

--- lib/statewide_loader.rb
+++ lib/statewide_loader.rb
@@ -5,13 +5,17 @@ class StatewideTestingLoader
 
   def self.load_third_grade_students
     rows = CSV.readlines(path + '/3rd grade students scoring proficient or above on the CSAP_TCAP.csv', headers: true, header_converters: :symbol).map(&:to_h)
-    groups = group_by(rows)
+    grouped_rows = rows.group_by { |row|  row.fetch(:location) }
+
     @third_grade_test_scores_repo = {}
+    location_with_years = grouped_rows.map do |key, value|
+     @third_grade_test_scores_repo[key] = value.map {|row| [row.fetch(:timeframe).to_i, value.map {|row| [row.fetch(:score).downcase.to_sym, row.fetch(:data).to_f]}.to_h]}.to_h
+    end
+    @third_grade_test_scores_repo
     binding.pry
     repo_builder(@third_grade_test_scores_repo, groups, :integer)
   end
 
-  def self.group_by
+  load_third_grade_students
 
-  end
 end
