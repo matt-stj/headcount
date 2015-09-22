63ead178b178f81c515e72a3e995d3770cc4489f
Ross Edfort <rossedfort@gmail.com>
2015-09-21 18:06:32 -0600

    proficient for subject by race in year test passing

--- test/statewide_test.rb
+++ test/statewide_test.rb
@@ -1,6 +1,16 @@
 require_relative '../lib/district_repository'
+require 'pry'
 
 class StatewideTestingTest < Minitest::Test
+
+  def path
+    File.expand_path '../data', __dir__
+  end
+
+  def make_a_dr
+    @dr ||= DistrictRepository.from_csv(path)
+  end
+
   def test_it_loads_some_shit
     skip
     dr = DistrictRepository.new('/3rd grade students scoring proficient or above on the CSAP_TCAP.csv')
@@ -8,4 +18,12 @@ class StatewideTestingTest < Minitest::Test
     binding.pry
     assert third_grade_proficiency
   end
+
+  def test_proficient_for_subject_by_race_in_year
+    make_a_dr
+    colorado = @dr.find_by_name("COLORADO")
+
+    assert_equal 0.656, colorado.statewide_testing.proficient_for_subject_by_race_in_year(:writing, :asian, 2011)
+  end
+
 end
